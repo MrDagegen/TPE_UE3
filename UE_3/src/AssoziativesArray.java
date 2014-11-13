f9c418b1cdd7e2dd3e68ab0981cfc91d2548edf2
marcel <marcel.eberling@gmx.net>
2014-11-13 13:19:59 +0100

    Neuer shit

--- UE_3/src/AssoziativesArray.java
+++ UE_3/src/AssoziativesArray.java
@@ -1,6 +1,12 @@
 
-public abstract class AssoziativesArray {
+public abstract class AssoziativesArray<T, U> {
 
-	// To Do
+	private T t;
+	private U u;
+	
+	public AssoziativesArray( T t, U u){
+		this.t = t;
+		this.u = u;
+	}
 	
 }
